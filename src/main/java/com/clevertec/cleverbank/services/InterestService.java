e1ee3485d8385c5e97b62a5ee66079600574df71
Karatkevich Aliaksei <akaratkevich2003@gmail.com>
2023-08-29 14:03:31 +0300

    InterestService mb done

--- src/main/java/com/clevertec/cleverbank/services/InterestService.java
+++ src/main/java/com/clevertec/cleverbank/services/InterestService.java
@@ -8,13 +8,14 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Серви для расчета и начисления процентов на балансы аккаунтов.
  */
 public class InterestService {
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private final ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);;
     private final AccountRepositoryImpl accountRepository;
     private final AccountServiceImpl accountService;
 
@@ -27,21 +28,28 @@ public class InterestService {
      * Запускает расчет и начисление процентов на балансы аккаунтов каждые 30 секунд.
      */
     public void startInterestCalculation() {
-        scheduler.scheduleAtFixedRate(this::calculateInterest, 0, 30, TimeUnit.SECONDS);
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(this::scheduleInterestCalculation, 0, 30, TimeUnit.SECONDS);
+    }
+
+    private void scheduleInterestCalculation() {
+        executor.submit(this::calculateInterest);
     }
 
     /**
      * Выполняет расчет и начисление процентов на балансы аккаунтов.
      */
     private void calculateInterest() {
-
         List<Account> accounts = accountRepository.getAllAccounts();
-        for (Account account : accounts) {
-            BigDecimal balance = account.getBalance();
-            BigDecimal interestRate = ConfigReader.getInterestRate();
-            BigDecimal interestAmount = balance.multiply(interestRate);
 
-            accountService.deposit(account.getId(), interestAmount);
+        for (Account account : accounts) {
+            executor.submit(() -> {
+                BigDecimal balance = account.getBalance();
+                BigDecimal interestRate = ConfigReader.getInterestRate();
+                BigDecimal interestAmount = balance.multiply(interestRate);
+
+                accountService.deposit(account.getId(), interestAmount);
+            });
         }
     }
 }
